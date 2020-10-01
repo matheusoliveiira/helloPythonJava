a721709cbc6f2a194a9dbda5a6171ff89cca34cc
Matheus Oliveira <71906492+matheusoliveiira@users.noreply.github.com>
2020-10-01 16:02:38 -0300

    forJava

---
+++ forJava.js
@@ -0,0 +1,9 @@
+//ensinando formas de for em java
+
+public class forJava{
+  public static void main(String[] args){
+    for(int i=0; i<10; i++){
+      System.out.println('Número: ' + (i+1));
+    }
+  }
+}
